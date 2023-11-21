65b6329598ec387ba69aa9cf183200debe199132
Evan Swanson <evan.sundelius@gmail.com>
2023-11-21 13:22:31 -0600

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="text-3xl font-bold italic text-green-400">
+            Hello world!
+          </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
